b67b3cb6add86d585eab86f446f1a976e5f42ecb
Adrian Kostya <akostya@solvd.com>
2022-07-04 00:36:52 +0200

    fixed chromeDriver location

--- src/main/java/com/online/shop/gui/services/Driver.java
+++ src/main/java/com/online/shop/gui/services/Driver.java
@@ -2,17 +2,30 @@ package com.online.shop.gui.services;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
 
 public class Driver {
 
     public static WebDriver driver;
 
-    @BeforeMethod
-    public void beforeTest(){
-        System.setProperty("webdriver.chrome.driver", "/Users/adriankostya/selenium/chromedriver");
-        this.driver = new ChromeDriver();
+    @BeforeTest
+    public void before(){
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
     @AfterMethod
