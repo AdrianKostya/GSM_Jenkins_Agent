a9b574b1898ee333b2d8bd232d3f18356108abca
Adrian Kostya <akostya@solvd.com>
2022-07-05 22:23:34 +0200

    asdf

--- src/main/java/com/online/shop/gui/services/Driver.java
+++ src/main/java/com/online/shop/gui/services/Driver.java
@@ -24,7 +24,7 @@ public class Driver {
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 
