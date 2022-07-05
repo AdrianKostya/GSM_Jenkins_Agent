83c9372bd200eb6dbb97cff41e0aba7f3821ef12
Adrian Kostya <akostya@solvd.com>
2022-07-05 23:13:16 +0200

    asdfasdf

--- src/main/java/com/online/shop/gui/services/Driver.java
+++ src/main/java/com/online/shop/gui/services/Driver.java
@@ -23,7 +23,7 @@ public class Driver {
         options.addArguments("--disable-gpu"); // applicable to windows os only
         options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
