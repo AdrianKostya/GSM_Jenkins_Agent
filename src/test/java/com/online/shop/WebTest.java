3b6d8e6afbc70436f1af4b0d3dda8694515bce5c
Adrian Kostya <akostya@solvd.com>
2022-07-06 11:23:00 +0200

    leaved only 1 test

--- src/test/java/com/online/shop/WebTest.java
+++ src/test/java/com/online/shop/WebTest.java
@@ -8,15 +8,7 @@ import java.text.ParseException;
 
 public class WebTest extends Driver {
 
-//    @Test
-//    public void verifyWrongLogin(){
-//        HomePage homePage = new HomePage(driver);
-//        homePage.open();
-//        homePage.clickAcceptCokies();
-//        Assert.assertTrue(homePage.isPageOpened());
-//        LoginPage loginPage = homePage.getHeader().openLoginForm().wrongLogin();
-//        Assert.assertTrue(loginPage.isLoginFailed());
-//    }
+
 
     @Test
     public void verifySuccessLogin(){
@@ -28,37 +20,7 @@ public class WebTest extends Driver {
         Assert.assertTrue(loginPage.isLoginSuccesful());
     }
 
-//    @Test
-//    public void verifyCommentPosted(){
-//        String nickname = "Bruno22";
-//        String commentTxt = "Like design";
-//        String brand ="Lenovo";
-//        HomePage homePage = new HomePage(driver);
-//        homePage.open();
-//        Assert.assertTrue(homePage.isPageOpened());
-//        homePage.clickAcceptCokies();
-//        homePage.getHeader().openLoginForm().correctLogin();
-//        ModelListPage modelListPage = homePage.getBrand(brand);
-//        LeaveOpinionPage leaveOpinionPage = modelListPage.getFirstModel().clickOpinionBtn();
-//        leaveOpinionPage.setComment(commentTxt);
-//        Assert.assertTrue(leaveOpinionPage.isSuccessfulPostedMsgPresent());
-//        homePage.getHeader().logOut();
-//        //Here i log again
-//        homePage.getHeader().openLoginForm().correctLogin();
-//        modelListPage = homePage.getBrand(brand);
-//        OpinionPage opinionPage =modelListPage.getFirstModel().getOpinionPage();
-//        String postedComment = opinionPage.getTxt(nickname);
-//        Assert.assertEquals(postedComment, commentTxt);
-//    }
 
-//    @Test
-//    public void getData() throws ParseException {
-//      HomePage homePage = new HomePage(driver);
-//      homePage.open();
-//      homePage.clickAcceptCokies();
-//      ModelListPage modelListPage = homePage.getBrand("Apple");
-//      Assert.assertTrue(modelListPage.getFirstModel().getOpinionPage().isDateSorted());
-//    }
 
 
 }
