aee5c821aa76f9b563760301b62c6640dde253e1
Adrian Kostya <akostya@solvd.com>
2022-07-06 13:55:20 +0200

    uncomented +1 logintest

--- src/test/java/com/online/shop/WebTest.java
+++ src/test/java/com/online/shop/WebTest.java
@@ -8,15 +8,15 @@ import java.text.ParseException;
 
 public class WebTest extends Driver {
 
-//    @Test
-//    public void verifyWrongLogin(){
-//        HomePage homePage = new HomePage(driver);
-//        homePage.open();
-//        homePage.clickAcceptCokies();
-//        Assert.assertTrue(homePage.isPageOpened());
-//        LoginPage loginPage = homePage.getHeader().openLoginForm().wrongLogin();
-//        Assert.assertTrue(loginPage.isLoginFailed());
-//    }
+    @Test
+    public void verifyWrongLogin(){
+        HomePage homePage = new HomePage(driver);
+        homePage.open();
+        //homePage.clickAcceptCokies();
+        Assert.assertTrue(homePage.isPageOpened());
+        LoginPage loginPage = homePage.getHeader().openLoginForm().wrongLogin();
+        Assert.assertTrue(loginPage.isLoginFailed());
+    }
 
     @Test
     public void verifySuccessLogin(){
