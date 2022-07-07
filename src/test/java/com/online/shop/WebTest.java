ec445e75d5a7c97dc993ceb632eaac9f00e197a5
Adrian Kostya <akostya@solvd.com>
2022-07-07 09:04:10 +0200

    commented text verify coment posted

--- src/test/java/com/online/shop/WebTest.java
+++ src/test/java/com/online/shop/WebTest.java
@@ -29,28 +29,28 @@ public class WebTest extends Driver {
         Assert.assertTrue(loginPage.isLoginSuccesful());
     }
 
-    @Test
-    public void verifyCommentPosted(){
-        String nickname = "Bruno22";
-        String commentTxt = "Like design";
-        String brand ="Samsung";
-        HomePage homePage = new HomePage(driver);
-        homePage.open();
-        Assert.assertTrue(homePage.isPageOpened());
-        //homePage.clickAcceptCokies();
-        homePage.getHeader().openLoginForm().correctLogin();
-        ModelListPage modelListPage = homePage.getBrand(brand);
-        LeaveOpinionPage leaveOpinionPage = modelListPage.getFirstModel().clickOpinionBtn();
-        leaveOpinionPage.setComment(commentTxt);
-        Assert.assertTrue(leaveOpinionPage.isSuccessfulPostedMsgPresent());
-        homePage.getHeader().logOut();
-        //Here i log again
-        homePage.getHeader().openLoginForm().correctLogin();
-        modelListPage = homePage.getBrand(brand);
-        OpinionPage opinionPage =modelListPage.getFirstModel().getOpinionPage();
-        String postedComment = opinionPage.getTxt(nickname);
-        Assert.assertEquals(postedComment, commentTxt);
-    }
+//    @Test
+//    public void verifyCommentPosted(){
+//        String nickname = "Bruno22";
+//        String commentTxt = "Like design";
+//        String brand ="Samsung";
+//        HomePage homePage = new HomePage(driver);
+//        homePage.open();
+//        Assert.assertTrue(homePage.isPageOpened());
+//        //homePage.clickAcceptCokies();
+//        homePage.getHeader().openLoginForm().correctLogin();
+//        ModelListPage modelListPage = homePage.getBrand(brand);
+//        LeaveOpinionPage leaveOpinionPage = modelListPage.getFirstModel().clickOpinionBtn();
+//        leaveOpinionPage.setComment(commentTxt);
+//        Assert.assertTrue(leaveOpinionPage.isSuccessfulPostedMsgPresent());
+//        homePage.getHeader().logOut();
+//        //Here i log again
+//        homePage.getHeader().openLoginForm().correctLogin();
+//        modelListPage = homePage.getBrand(brand);
+//        OpinionPage opinionPage =modelListPage.getFirstModel().getOpinionPage();
+//        String postedComment = opinionPage.getTxt(nickname);
+//        Assert.assertEquals(postedComment, commentTxt);
+//    }
 
     @Test
     public void getData() throws ParseException {
