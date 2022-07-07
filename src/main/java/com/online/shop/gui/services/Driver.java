de590a5a44a5ce3e10a9ff0145bacf181900e62f
Adrian Kostya <akostya@solvd.com>
2022-07-07 08:32:29 +0200

    deleted chrome options  remind

--- src/main/java/com/online/shop/gui/services/Driver.java
+++ src/main/java/com/online/shop/gui/services/Driver.java
@@ -15,14 +15,7 @@ public class Driver {
     public void before(){
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized"); // open Browser in maximized mode
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("--no-sandbox");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
